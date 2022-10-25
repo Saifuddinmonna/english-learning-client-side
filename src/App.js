9e8c3a2dcd96459fdcaa4dfb38b2c3c7f468be89
Saifuddinmonna <Saifuddinmonna@gmail.com>
2022-10-25 19:23:02 +0600

    nabvar added successfully

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/Home/Home";
 import Courses from "./components/Courses/Courses";
@@ -6,49 +5,44 @@ import Blog from "./components/Blog/Blog";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "./components/Main/Main";
+import Header from "./components/Header/Header";
 
 function App() {
 	const router = createBrowserRouter([
 		{
 			path: "/",
-			element: <Main />,
+			element: <Main></Main>,
 			children: [
 				{
 					path: "/home",
 
 					element: <Home></Home>,
 				},
-				{
-					path: "/home",
 
-					element: <Home></Home>,
+				{
+					path: "*",
+					element: <ErrorPage></ErrorPage>,
 				},
-				{
-					path: "/home",
 
-					element: <Home></Home>,
+				{
+					path: "/blog",
+					element: <Blog></Blog>,
 				},
 				{
 					path: "/courses",
 					loader: async () => fetch("products.json"),
 					element: <Courses></Courses>,
 				},
-
-				{
-					path: "*",
-					element: <ErrorPage></ErrorPage>,
-				},
 			],
-		},
-		{
-			path: "/blog",
-			element: <Blog></Blog>,
 		},
 	]);
 
 	return (
 		<div className="App">
-			<RouterProvider router={router}></RouterProvider>
+			<RouterProvider router={router}>
+				
+				
+			</RouterProvider>
 		</div>
 	);
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,39 @@
 import React from "react";
+import "./Header.css";
+
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
-import { NavLink } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
 	return (
 		<Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
 			<Container>
-				<Navbar.Brand href="/home">English Learning</Navbar.Brand>
+				<Navbar.Brand to="/home">English Learning <span> <img src="" alt="" /></span></Navbar.Brand>
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 				<Navbar.Collapse id="responsive-navbar-nav">
 					<Nav className="me-auto">
-						<NavLink to="#/">Features</NavLink>
-						<NavLink to="#/courses">Pricing</NavLink>
-						<NavLink to="#/blog">Pricing</NavLink>
-						
+						<Nav.Link className="text-white">
+							<NavLink
+								variant="dark"
+								className="Nav.Link px-2 text-decoration-none text-white "
+								to="/home">
+								Home
+							</NavLink>
+							<NavLink
+								className="px-2 text-decoration-none text-white"
+								to="/courses">
+								Courses
+							</NavLink>
+							<NavLink
+								className="px-2 text-decoration-none text-white"
+								to="/blog">
+								Blog
+							</NavLink>
+						</Nav.Link>
 					</Nav>
 					<Nav>
 						<Nav.Link href="#deets">Light</Nav.Link>
