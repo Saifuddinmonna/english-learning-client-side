f67f61a4d45e5b7f5a17d3f0e47e508a11f8fe44
Saifuddinmonna <Saifuddinmonna@gmail.com>
2022-10-27 05:54:52 +0600

    responsive ness and faq added successfully

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -6,34 +6,46 @@ import Tooltip from "react-bootstrap/Tooltip";
 
 const FAQ = () => {
 	return (
-		<div className="text-dark bg-orange-700 m-5">
-			<h1>This is FAQ</h1>
-			<div
-				className="tooltip tooltip-open tooltip-bottom"
-				data-tip="hello">
-				<button className="btn">Bottom</button>
-				<>
-					{["top", "right", "bottom", "left"].map((placement) => (
-						<d
-							key={placement}
-			    		placement={placement}
-							overlay={
-								<Tooltip id={`tooltip-${placement}`}>
-									Tooltip on <strong>{placement}</strong>.
-								</Tooltip>
-							}>
-							<Button variant="white">
-								Tooltip on {placement}
-							</Button>
-						</d>
-					))}
-				</>
+		<div className="text-dark bg-stone-50 container rounded-md m-5 p-4 shadow-lg">
+			<h1 className="border shadow rounded-full p-3">This is FAQ</h1>
+			<div>
+				<h5 className="border shadow rounded-full p-3 text-left line-height">
+					How does NodeJS works internally?
+					<br /> What does node actually do? <br />
+					Is Node a web server? <br />
+					Is Node.js backend or middleware? Is node multithreaded?
+					<br />
+					What is an API node? Why node js is called node?
+					<p>
+						How does node Express work? <br /> Express is a node js
+						web application framework that provides broad features
+						for building web and mobile applications. It is used to
+						build a single page, multipage, and hybrid web
+						application. It's a layer built on the top of the Node
+						js that helps manage servers and routes.Oct 13, 2022
+						Express JS Tutorial: br What is Express in Node JS?
+						tutorials › <br />
+						what-is-express-js Search for: How does node Express
+						work? How does node execute?
+						<br /> Node. js takes a different approach. It runs a
+						single-threaded event loop registered with the system to
+						handle connections, and each new connection causes a
+						JavaScript callback function to fire.Oct 1, 2022 What is
+						Node.js? The JavaScript runtime explained - <br />
+						Software Development Search for: How does node execute?
+						How does NodeJS works internally?
+						<br />
+						Node JS Web Server internally maintains a Limited Thread
+						pool to provide services to the Client Requests. Node JS
+						Web Server receives those requests and places them into
+						a Queue. It is known as “Event Queue”. Node JS Web
+						Server internally has a Component, known as “Event
+						Loop”.Aug 3, 2022 Node JS Architecture - Single Threaded
+						Event Loop
+					</p>
+				</h5>
 			</div>
-
-			
 		</div>
-
-
 	);
 };
 
