cf8056991d833f1d31b34960e0d0de60d412359a
Saifuddinmonna <Saifuddinmonna@gmail.com>
2022-10-27 07:05:07 +0600

    git moredetails asn faq updated

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -6,44 +6,233 @@ import Tooltip from "react-bootstrap/Tooltip";
 
 const FAQ = () => {
 	return (
-		<div className="text-dark bg-stone-50 container rounded-md m-5 p-4 shadow-lg">
-			<h1 className="border shadow rounded-full p-3">This is FAQ</h1>
-			<div>
-				<h5 className="border shadow rounded-full p-3 text-left line-height">
-					How does NodeJS works internally?
-					<br /> What does node actually do? <br />
-					Is Node a web server? <br />
-					Is Node.js backend or middleware? Is node multithreaded?
-					<br />
-					What is an API node? Why node js is called node?
-					<p>
-						How does node Express work? <br /> Express is a node js
-						web application framework that provides broad features
-						for building web and mobile applications. It is used to
-						build a single page, multipage, and hybrid web
-						application. It's a layer built on the top of the Node
-						js that helps manage servers and routes.Oct 13, 2022
-						Express JS Tutorial: br What is Express in Node JS?
-						tutorials › <br />
-						what-is-express-js Search for: How does node Express
-						work? How does node execute?
-						<br /> Node. js takes a different approach. It runs a
-						single-threaded event loop registered with the system to
-						handle connections, and each new connection causes a
-						JavaScript callback function to fire.Oct 1, 2022 What is
-						Node.js? The JavaScript runtime explained - <br />
-						Software Development Search for: How does node execute?
-						How does NodeJS works internally?
-						<br />
-						Node JS Web Server internally maintains a Limited Thread
-						pool to provide services to the Client Requests. Node JS
-						Web Server receives those requests and places them into
-						a Queue. It is known as “Event Queue”. Node JS Web
-						Server internally has a Component, known as “Event
-						Loop”.Aug 3, 2022 Node JS Architecture - Single Threaded
-						Event Loop
-					</p>
-				</h5>
+		<div className="text-dark text-justify bg-stone-50 container rounded-md m-5 p-4 shadow-lg">
+			<h1 className="border shadow rounded-full p-3">
+				Frequently asked questions
+			</h1>
+
+			<div className="border shadow rounded-md bg-red-50 p-3 m-3 ">
+				<h3 className="text-capitalize text-primary">
+					Learn more about English Language
+				</h3>
+
+				<p className=" border rounded shadow p-3 m-3">
+					English is the standard language for international commerce,
+					and is one of the most commonly spoken languages worldwide.
+					Fluency in spoken English makes it easier to travel and do
+					business with people in English speaking countries. An
+					English course on Udemy can even help improve your
+					employability in many global industries. English classes on
+					Udemy focus on grammar, pronunciation, and conversation
+					skills to help you gain confidence and fluency.
+				</p>
+			</div>
+
+			<div className="border shadow rounded-md bg-red-50 p-3 m-3 ">
+				<h3 className="text-capitalize text-primary">
+					is the english language difficult to learn?
+				</h3>
+				<p className=" border rounded shadow p-3 m-3"></p>Some aspects
+				of English are more challenging than others. For example, some
+				English language learners generally find tenses easy but may
+				sometimes struggle with pronunciation. Luckily, you can immerse
+				yourself in an English-speaking environment, no matter where you
+				are. It is very easy to practice English; examples include
+				speaking with native English speakers, taking an online English
+				language course, or immersing yourself by traveling to an
+				English-speaking country. Watching English-language TV and
+				listening to English-language music and news are also excellent
+				ways of immersing yourself in the language and improving your
+				English skills.
+			</div>
+			<div className="border shadow rounded-md bg-red-50 p-3 m-3 ">
+				<h3 className="text-capitalize text-primary">
+					Where is the english laguage spoken?
+				</h3>
+				<p className=" border rounded shadow p-3 m-3"></p>English is the
+				official language of 55 countries. It is the official language
+				in well-known countries like England, Australia, India, the
+				United States, Canada, and New Zealand. But it is also the
+				official language in many smaller countries, such as Botswana,
+				Antigua, Barbados, Gibraltar, Granada, and many more. There are
+				over 300 million native speakers of English and almost 750
+				million non-native speakers of English. That's over one billion
+				speakers of English worldwide. So, while English is not the most
+				spoken language on the planet, it certainly is the most
+				widespread. In addition, world travelers often communicate in
+				English if they don't share a native language.
+			</div>
+			<div className="border shadow rounded-md bg-red-50 p-3 m-3 ">
+				<h3 className="text-capitalize text-primary">
+					What are the origin od english language?
+				</h3>
+
+				<p className=" border rounded shadow p-3 m-3">
+					n many ways, the history of the English language is the
+					history of the British Isles. Every invading and occupying
+					person left its mark on the English language. English is a
+					Germanic language that includes influences from Greek and
+					Roman, with a healthy dose of French thrown in. There is
+					also a smattering of Dutch and Norse influence and even a
+					little bit of Urdu. Like many other languages, English
+					borrows words from other languages. But, unlike many other
+					languages, English preserves the spelling patterns and the
+					pronunciations of words it borrows from other languages.
+				</p>
+			</div>
+			<div className="border shadow rounded-md bg-red-50 p-3 m-3 ">
+				<h3 className="text-capitalize text-primary">
+					What are the major word class in the englidh language and
+					what are their signiicant?
+				</h3>
+
+				<p className=" border rounded shadow p-3 m-3">
+					English has four major word classes: nouns, verbs,
+					adjectives, and adverbs. Of these four classes, nouns are
+					the largest, followed closely by verbs. Nouns are people,
+					places, and things, such as cars, island, or engineer. Verbs
+					are actions, including to write, to listen, and to drive.
+					Adjectives are words that modify nouns: a green car, a smart
+					engineer. Finally, adverbs can modify verbs and adjectives:
+					walk quickly, a very tired puppy. However, there's more to
+					it than just those four word classes. English is unique in a
+					couple of ways. First, English speakers sometimes create new
+					words by turning nouns into verbs. For example, in the
+					sentence "We mothballed the project," the word mothball
+					(originally a noun) was turned into a verb. Second,
+					especially in everyday spoken English, people often use
+					adjectives instead of adverbs. For example, "you drive too
+					fast," where fast (an
+				</p>
+			</div>
+			<div className="border shadow rounded-md bg-red-50 p-3 m-3 ">
+				<h3 className="text-capitalize text-primary">
+					Whaat are the majors dailects and varieties of the english
+					language?
+				</h3>
+
+				<p className=" border rounded shadow p-3 m-3">
+					American English is the most widely heard dialect of English
+					because the United States is where many TV shows, movies,
+					and music are made. However, millions of people speak
+					British English. Australian and New Zealand English are also
+					very common dialects, as is South African English. In terms
+					of sheer numbers of speakers, Indian English and Pakistani
+					English are very widespread. While people who speak
+					different English dialects can usually mostly understand
+					each other, they might have to be careful not to use too
+					many idioms.
+				</p>
+			</div>
+			<div className="border shadow rounded-md bg-red-50 p-3 m-3 ">
+				<h3 className="text-capitalize text-primary">
+					Why english is an important language?
+				</h3>
+
+				<p className=" border rounded shadow p-3 m-3">
+					Increasingly, English has become a leading language for
+					business, travel, science, and many other things. On the
+					internet, over 25% of websites are in English. Anyone who
+					hopes to do business outside of their country would find
+					English helpful to communicate with people worldwide.
+					Knowing English is often a requirement for people who wish
+					to work in the hospitality industry. People who travel the
+					world often rely on English to communicate with others,
+					regardless of what country they're in. And that's just the
+					beginning! Many forms of entertainment, including movies, TV
+					shows, apps, video games, etc., are in English. As the world
+					becomes more and more connected, it has relied more and more
+					heavily on English as a shared language.
+				</p>
+			</div>
+			<div className="border shadow rounded-md bg-red-50 p-3 m-3 ">
+				<h3 className="text-capitalize text-primary"></h3>
+
+				<p className=" border rounded shadow p-3 m-3">
+					Learning English can open a wide variety of opportunities -
+					from studying in an English-speaking country through to
+					increased career options both within your home country and
+					abroad. As a globally important language, there’s never been
+					a better time to take a course on learning English. The
+					benefits of learning English As well as being the official
+					language of 53 countries, English is also spoken by around
+					400 million people worldwide and is the lingua franca in
+					business. It’s easy to see why so many people want to learn
+					and improve their skills in this language. With an online
+					course, you can do exactly that. Many educational
+					institutions are also offering courses taught in English.
+					So, if you want to study anywhere around the world, it can
+					be highly beneficial to improve your understanding of the
+					language so that you can understand course materials and
+					even take the IELTS test. Similarly, if you’re teaching
+					people English as a second language, it can be useful to
+					know how the various assessments work. By studying online,
+					you get access to all of the essential information you need
+					in one place. You can log on to your account from just about
+					anywhere, and study at a time that’s convenient for you.
+					Choosing an English language course FutureLearn’s selection
+					of learn English courses cover a diverse range of topics.
+					There are resources for students of English, covering topics
+					such as basic English and how to write in English for
+					university study. For those who are planning on taking an
+					IELTS qualification, you can find courses on understanding
+					the listening, writing, speaking and reading components.
+					These courses are also useful for those who are teaching
+					students preparing for the IELTS test. It’s not just
+					existing students who can benefit from taking an online
+					learn English course either. A course on English in the
+					workplace is suitable for job seekers and professionals who
+					would like to improve their subject-specific vocabulary. For
+					academics, you can find several courses that can boost your
+					credentials. With a course on English as a medium for
+					instruction, you can improve both your English skills and
+					your intercultural knowledge. How long does an English
+					course take? Many of our courses can be completed over a
+					period of a few weeks. The shortest tend to take around two
+					weeks to complete, with three hours of study required each
+					week. More intense courses can take up to six weeks,
+					requiring three to six hours of study each week. FAQs Can I
+					learn English online? With plenty of practice, you can learn
+					English online. You can join one of our courses to learn
+					vocabulary and practice your reading, writing, speaking and
+					listening skills. To supplement your learning, you can also
+					read books, watch TV, and listen to music in English. Our
+					courses are completely online, so you’re free to study
+					whenever suits you. You can also learn from anywhere in the
+					world on a digital device. How difficult is it to learn
+					English? The English language is considered to be one of the
+					most challenging languages to learn. There are lots of
+					inconsistencies, illogical rules, and difficult spellings.
+					However, everyone has the ability to learn. Our courses are
+					simple to follow and created by experts, so you’ll be in
+					good hands. Additionally, English is an accessible language
+					to learn because there are so many resources available to
+					help you practice. What are the top English language
+					courses? We have a wide selection of great English language
+					courses for you. If you’re a total beginner, our Basic
+					English 1: Elementary course by KCL is one of our most
+					popular courses ever, or you might want to practice for your
+					IELTS exam with our top-rated ExpertTrack from the British
+					Council. If you’re looking for something more specific, some
+					other highly rated courses we have available include Writing
+					in English for University Study, Learn English Through TV
+					Drama Series, and English for the Workplace, where you can
+					develop the language skills needed to find a job. How can
+					learning English help my career prospects? No matter where
+					you’re from, learning a new language is beneficial. Being
+					multilingual can improve your networking skills, negotiation
+					skills, and your understanding of international relations.
+					English specifically can be useful to learn because it is
+					spoken in so many different countries, so you’ll be able to
+					work anywhere in the world. You might even be eligible
+					className="border shadow rounded-md bg-red-50 p-3 m-3 "f
+				</p>
+			</div>
+			<div className="border shadow rounded-md bg-red-50 p-3 m-3 ">
+				<h3 className="text-capitalize text-primary"></h3>
+
+				<p className=" border rounded shadow p-3 m-3"></p>
+			
 			</div>
 		</div>
 	);
